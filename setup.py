f1c7ef148f1d3d1fd9aa3cc43f10c6a22afa852c
Gavin M. Roy <gavinmroy@gmail.com>
2015-01-04 17:10:39 -0500

    Update setup and support wheel dist

--- setup.py
+++ setup.py
@@ -1,32 +1,39 @@
 from distutils.core import setup
 import sys
 
-tests_require = []
-if sys.version_info[0:2] == (2,6):
-      test_require = ['unittest2']
+from flatdict import __version__
+
+tests_require = ['nose']
+if sys.version_info < (2, 7, 0):
+    tests_require.append('unittest2')
+
+classifiers = ['Development Status :: 5 - Production/Stable',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: POSIX',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.2',
+               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: Implementation :: CPython',
+               'Programming Language :: Python :: Implementation :: PyPy',
+               'Topic :: Software Development :: Libraries']
 
 setup(name='flatdict',
-      version='1.1.1',
+      version=__version__,
       description=('Python module for interacting with nested dicts as a '
                    'single level dict with delimited keys.'),
       long_description=open('README.rst').read(),
       author='Gavin M. Roy',
-      author_email='gmr@meetme.com',
-      license='https://github.com/gmr/flatdict/blob/master/LICENSE',
+      author_email='gavinmroy@gmail.com',
       url='http://github.com/gmr/flatdict',
+      package_data={'': ['LICENSE', 'README.rst']},
       tests_require=tests_require,
       py_modules=['flatdict'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: POSIX',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.2',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: Implementation :: CPython',
-                   'Programming Language :: Python :: Implementation :: PyPy',
-                   'Topic :: Software Development :: Libraries'])
+      license=open('LICENSE').read(),
+      classifiers=classifiers,
+      zip_safe=True)
