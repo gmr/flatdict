8a7fb8ad449dedc7b69d2874b8b1df3d47fa440a
Gavin M. Roy <gavinr@aweber.com>
2019-06-10 11:48:43 -0400

    Specify the master_doc for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import sys
 import pkg_resources
 
 sys.path.insert(0, os.path.abspath('..'))
-
+master_doc = 'index'
 project = 'flatdict'
 release = version = pkg_resources.get_distribution(project).version
 copyright = '{}, Gavin M. Roy'.format(datetime.date.today().year)
