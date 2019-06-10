44d1ed80ff2f9fba5bdef1fe49e50b03b3727d2f
Gavin M. Roy <gavinr@aweber.com>
2019-06-10 10:28:37 -0400

    Update how coverage is combined to reduce errors

---
+++ ci/upload-coverage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -e
+pip install awscli
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]
+then
+    echo "AWS_ACCESS_KEY_ID is not set, exiting"
+    exit 0
+fi
+aws s3 cp .coverage "s3://com-gavinroy-travis/flatdict/${TRAVIS_BUILD_NUMBER}/.coverage.${TRAVIS_PYTHON_VERSION}"
