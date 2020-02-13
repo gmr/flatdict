42cdcb1b51bb38f2df6376379ea3016f27661cf9
Gavin M. Roy <gavinr@aweber.com>
2020-02-13 14:12:29 -0500

    Actually install

--- setup.py
+++ setup.py
@@ -4,3 +4,4 @@ import setuptools
 setuptools_version = pkg_resources.parse_version(setuptools.__version__)
 if setuptools_version < pkg_resources.parse_version('39.2'):
     raise SystemExit('Please upgrade setuptools')
+setuptools.setup()
