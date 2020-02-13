f86186671944e39ae194b49c5f444d69ffa37424
Gavin M. Roy <gavinr@aweber.com>
2020-02-13 14:15:00 -0500

    Be more specific in the exit message

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import setuptools
 
 setuptools_version = pkg_resources.parse_version(setuptools.__version__)
 if setuptools_version < pkg_resources.parse_version('39.2'):
-    raise SystemExit('Please upgrade setuptools')
+    raise SystemExit('setuptools 39.2 or greater required for installation')
 setuptools.setup()
